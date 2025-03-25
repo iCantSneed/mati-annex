0ecd12f6ce1a0ecdccd6532483425e1e2d3aa886
iCantSneed <140197533+iCantSneed@users.noreply.github.com>
2025-03-25 17:34:53 +0000

    stash

--- config/bundles.php
+++ config/bundles.php
@@ -3,9 +3,11 @@
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MakerBundle\MakerBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 
 return [
   FrameworkBundle::class => ['all' => true],
   MakerBundle::class => ['dev' => true],
   MonologBundle::class => ['all' => true],
+  TwigBundle::class => ['all' => true],
 ];
--- src/Controller/LiveController.php
+++ src/Controller/LiveController.php
@@ -3,21 +3,35 @@
 namespace Mati\Controller;
 
 use Mati\Rumble\RumbleHlsFetcher;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 final class LiveController extends AbstractController
 {
   #[Route('/live', name: 'app_live')]
-  public function live(RumbleHlsFetcher $rumbleHlsFetcher): Response
+  public function live(CacheInterface $cache, RumbleHlsFetcher $rumbleHlsFetcher): Response
   {
-    $hlsUrl = $rumbleHlsFetcher->fetch();
-    if (null === $hlsUrl) {
-      // TODO
-      return new Response();
-    }
+    $hlsUrl = $cache->get('rumble_hls_url', function (CacheItemInterface $cacheItem) use ($rumbleHlsFetcher) {
+      $cacheItem->expiresAfter(600);
+      $hlsUrl = $rumbleHlsFetcher->fetch();
+      if (null === $hlsUrl) {
+        return $this->generateUrl('app_nonlive_chunklist');
+      }
+
+      return $hlsUrl;
+    });
 
     return $this->redirect($hlsUrl);
+  }
+
+  #[Route('/nonlive/chunklist.m3u8', name: 'app_nonlive_chunklist')]
+  public function nonliveChunklist(): Response
+  {
+    return $this->render('nonlive/chunklist.m3u8.twig', [], new Response(null, 200, [
+      'Content-Type' => 'application/vnd.apple.mpegurl.audio',
+    ]));
   }
 }
